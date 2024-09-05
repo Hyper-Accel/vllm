3dffa224f31584cd39d0ac5a0c7973fb6bcea80a
hyunjun <hyunjun3595@yonsei.ac.kr>
2024-09-05 02:12:27 -0400

    Support stop serving system

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -806,6 +806,7 @@ class AsyncLLMEngine:
         if self._background_loop_unshielded is not None:
             self._background_loop_unshielded.cancel()
             self._background_loop_unshielded = None
+            print_logger("shutdown")
         self.background_loop = None
 
     def _init_engine(self, *args,
@@ -934,8 +935,12 @@ class AsyncLLMEngine:
                             asyncio.create_task(
                                 self.engine_step(virtual_engine)))
                         has_requests_in_progress[virtual_engine] = True
+                        print_logger(has_unfinished_requests)
                     else:
                         has_requests_in_progress[virtual_engine] = False
+                        self.engine.model_executor.cleanup()
+                        #self.engine.execute_model.cleanup()
+                        print_logger(has_unfinished_requests)
             except asyncio.TimeoutError as exc:
                 logger.error(
                     "Engine iteration timed out. This should never happen!")
@@ -1222,6 +1227,7 @@ class AsyncLLMEngine:
         t = time.perf_counter()
         logger.debug("Starting health check...")
         if self.is_stopped:
+            print_logger("is_stopped")
             raise AsyncEngineDeadError("Background loop is stopped.")
 
         if self.engine_use_ray:
