baf656ff04ed146fe8d255315af57801538d4437
hyunjun <hyunjun3595@yonsei.ac.kr>
2024-09-10 09:57:10 +0900

    Add install script

---
+++ scripts/install_script.sh
@@ -0,0 +1,10 @@
+
+site_packages=$(python3 -c "import site; print(site.getsitepackages()[0])")
+
+pip install vllm==0.5.5
+pip install mistral_common
+echo "Start move vllm to ${site_packages}"
+sudo mv ${site_packages}/vllm ${site_packages}/vllm_bk
+echo "Done backup"
+sudo cp -r ../vllm ${site_packages}/vllm
+echo "Done copy"
