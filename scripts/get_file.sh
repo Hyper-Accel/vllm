7439ec643c29736c2bee952e46c70727b8ebe756
hyunjun <hyunjun3595@yonsei.ac.kr>
2024-11-13 13:51:10 +0900

    Modify Script

--- scripts/get_file.sh
+++
@@ -1,7 +0,0 @@
-
-site_packages=$(python3 -c "import site; print(site.getsitepackages()[0])")
-
-cp ${site_packages}/vllm/_C.abi3.so .
-cp ${site_packages}/vllm/commit_id.py .
-cp ${site_packages}/vllm/_core_C.abi3.so .
-cp ${site_packages}/vllm/_moe_C.abi3.so .
--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -2,10 +2,14 @@
 site_packages=$(python3 -c "import site; print(site.getsitepackages()[0])")
 
 pip install numpy==1.26.0
-pip install vllm==0.5.5
+pip install vllm==0.6.1
 pip install mistral_common
 echo "Start move vllm to ${site_packages}"
-sudo mv ${site_packages}/vllm ${site_packages}/vllm_bk
+mv ${site_packages}/vllm ${site_packages}/vllm_bk
 echo "Done backup"
-sudo cp -r ../vllm ${site_packages}/vllm
+cp -r ../vllm ${site_packages}/vllm
+cp ${site_packages}/vllm_bk/_C.abi3.so ${site_packages}/vllm/
+cp ${site_packages}/vllm_bk/_moe_C.abi3.so ${site_packages}/vllm/
+cp ${site_packages}/vllm_bk/_core_C.abi3.so ${site_packages}/vllm/
 echo "Done copy"
+
